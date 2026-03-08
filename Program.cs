94200fdadb75be90489dd8742c5e6125a4bbe0d5
ialikh72 <ialikh.w1@gmail.com>
2026-03-08 21:59:20 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        bool a, b;
+        Console.WriteLine("XOR");
+        for (int i = 0; i < 2; i++)
+        {
+            a = (i == 1);
+            for (int j = 0; j < 2; j++)
+            {
+                b = (j == 1);
+                Console.WriteLine(a + "^" + b + "=" + (a ^ b));
+            }
+        }
+        Console.WriteLine("NOT");
+        for (int i = 0; i < 2; i++)
+        {
+            a = (i == 1);
+            Console.WriteLine(a + "=" + !a);
+        }
+    }
+}
